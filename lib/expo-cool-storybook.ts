85a631ff85c93c662a86b1485beef7d1a634f34b
Calum MacRae <calum.macrae@medayo.co.uk>
2018-10-15 16:12:28 +0100

    chore: Making it easier

--- lib/expo-cool-storybook.ts
+++ lib/expo-cool-storybook.ts
@@ -4,12 +4,14 @@ import { parse, version } from 'commander';
 import 'es6-shim';
 import 'reflect-metadata';
 import { WebsocketServer } from './server';
+
 version('0.1.0')
   .command('start [options...]')
   .option('-p, --port <n>', 'An integer argument', parseInt)
   .action((env, options: { port: number }) => {
     // tslint:disable-next-line:no-commented-code
     // start(options.port);
+    console.log('Starting websocket server on ', options.port);
     WebsocketServer.start(options.port);
   });
 
