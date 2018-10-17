89685016b105b5879c7c81e92be26519340bcb1f
Calum MacRae <calum.macrae@medayo.co.uk>
2018-10-17 02:26:09 +0100

    fix: Updated examples

--- example/TextInfo.tsx
+++ example/TextInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { CoolStorybookService } from '../lib/client/snapshot.service';
 import { addStoriesOf, CreateStory } from '../lib/client/stories.component';
+import { PhoneSizeTypes } from '../lib/client/story.storage';
 
 interface ITextInfo {
     children: string;
@@ -9,15 +10,20 @@ interface ITextInfo {
 
 @CreateStory('This tests the component with text "Expo is awesome"',
     () => <ExampleText>Expo is awesome</ExampleText>,
-    { transparent: false })
+    { style: PhoneSizeTypes.PhoneCentered })
 export class ExampleText extends React.Component<ITextInfo, any> {
     constructor(props: any) {
         super(props);
     }
 
     public componentDidMount() {
-        addStoriesOf('ExampleText', 'This was the render of: ' + this.props.children, () => this.render());
+        addStoriesOf('ExampleText',
+            'This was the render of: ' + this.props.children,
+            () => this.render(),
+            { style: PhoneSizeTypes.PhoneDimensions },
+        );
     }
+
     public render() {
         return (
             <Text>{this.props.children}</Text>
--- example/stories/text.story.tsx
+++ example/stories/text.story.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { storiesOf } from '../../lib/client';
+import { PhoneSizeTypes } from '../../lib/client/story.storage';
 
 storiesOf('Circlebutton', { transparent: false })
     .addDetail('with Text', () =>
         <Text>James</Text>,
-
+        { style: PhoneSizeTypes.PhoneCentered },
     )
     .addDetail('with Text 2', () =>
-        <Text>This is text212121221</Text>,
+        <Text>Cool</Text>,
     );
