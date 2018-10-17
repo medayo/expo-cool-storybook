8a104573f2617e3ea01db6eeaae85b999e2d4f24
Calum MacRae <calum.macrae@medayo.co.uk>
2018-10-17 01:51:03 +0100

    Examples!

--- example/TextInfo.tsx
+++ example/TextInfo.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { CoolStorybookService } from '../lib/client/snapshot.service';
-import { CreateStory } from '../lib/client/stories.component';
+import { addStoriesOf, CreateStory } from '../lib/client/stories.component';
 
-@CreateStory('This says gogogo5',
-    () => <TextInfo>testing</TextInfo>,
+interface ITextInfo {
+    children: string;
+}
+
+@CreateStory('This tests the component with text "Expo is awesome"',
+    () => <ExampleText>Expo is awesome</ExampleText>,
     { transparent: false })
-export class TextInfo extends React.Component<any, any> {
+export class ExampleText extends React.Component<ITextInfo, any> {
     constructor(props: any) {
         super(props);
     }
 
     public componentDidMount() {
-        CreateStory('This was the render', this.render, {}, 'TextInfo');
-        CoolStorybookService.sendStory('TextInfo');
-        CoolStorybookService.sendAllStories();
+        addStoriesOf('ExampleText', 'This was the render of: ' + this.props.children, () => this.render());
     }
     public render() {
         return (
